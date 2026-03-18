b857d5d52a8012d587e95b90bd9916461dfda313
Venice Ochoa <timaeustestified@Venices-MacBook-Air.local>
2026-03-18 10:48:37 +0800

    Overwrite download_data.py

--- download_data.py
+++ download_data.py
@@ -1,2 +1,15 @@
 #python script to automate data downloads
 #will be added in future
+
+#use a venv for this to run on macOS
+#to install dependencies
+import subprocess
+import sys
+def dependencies():
+    try:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    except Exception as e:
+        print(f"Error : {e}")
+        
+if __name__ == "__main__":
+    dependencies()
